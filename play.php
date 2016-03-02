60dee77ffc461d223ee03f93d1238e7b3d85ca69
nfs97 <vladronaldo7@gmail.com>
2016-03-03 00:39:16 +0200

    changed play.php

--- play.php
+++ play.php
@@ -3,6 +3,7 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @var Composer\Autoload\ClassLoader $loader
@@ -19,16 +20,15 @@ $container = $kernel->getContainer();
 
 $templating = $container->get('templating');
 
-echo $templating->render(
-    'EventBundle:Default:index.html.twig',
-    array('name'=>'Vader', 'count'=>10)
-);
 
-/*use EventBundle\Entity\Event;
+use EventBundle\Entity\Event;
 
 $event = new Event();
 $event->setName('Darth\'s surprise bithday party');
 $event->setLocation("DeathStar");
 $event->setTime(new \DateTime('tomorrow soon'));
 $event->setDetails('Ha! Darth hates surprises!!!');
-*/
+
+$em=$container->get('doctrine')->getManager();
+$em->persist();
+
