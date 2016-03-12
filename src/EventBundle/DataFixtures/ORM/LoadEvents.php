61ecf02b601465d0ce3eed7c8c9335b180d4c2cb
nfs97 <vladronaldo7@gmail.com>
2016-03-12 22:08:41 +0200

    Yeah! Finished lesson on Fixtures

---
+++ src/EventBundle/DataFixtures/ORM/LoadEvents.php
@@ -0,0 +1,29 @@
+<?php
+namespace EventBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use EventBundle\Entity\Event;
+
+class LoadEvents implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $event1 = new Event();
+        $event1->setName('Darth surprise bithday party and the guest is');
+        $event1->setLocation("DeathStar");
+        $event1->setTime(new \DateTime('2000-01-01'));
+        $event1->setDetails('Ha! Darth hates surprises!!! But give him presents');
+
+        $manager->persist($event1);
+
+        $event2 = new Event();
+        $event2->setName('Luke Skywalker ');
+        $event2->setLocation("yavin");
+        $event2->setTime(new \DateTime('2005-07-04'));
+        $event2->setDetails('Get there faster than I can do it');
+        $manager->persist($event2);
+
+        $manager->flush();
+    }
+}
