b63e7f0b71f51b907cb5a8ab7008cb6217487874
nfs97 <vladronaldo7@gmail.com>
2016-04-03 20:35:17 +0300

    customized some forms

--- src/UserBundle/Form/RegisterFormType.php
+++ src/UserBundle/Form/RegisterFormType.php
@@ -9,6 +9,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 
@@ -33,5 +35,11 @@ class RegisterFormType extends AbstractType
             'data_class' => 'UserBundle\Entity\Users',
             ));
     }
+
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        $view['email']->vars['help'] = 'Hint: It will have an @ symbol';
+    }
+
 
 }
